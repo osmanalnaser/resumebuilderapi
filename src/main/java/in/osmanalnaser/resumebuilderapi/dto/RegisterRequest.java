e7874fa525287935cf70cb1f0fb4ede705ef7517
osman <osman.n2@icloud.com>
2026-03-02 02:36:39 +0100

    feat: add RegisterRequest DTO with Bean Validation annotations

---
+++ src/main/java/in/osmanalnaser/resumebuilderapi/dto/RegisterRequest.java
@@ -0,0 +1,22 @@
+package in.osmanalnaser.resumebuilderapi.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
+    private String email;
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 15, message = "Name must be between 2 and 15")
+    private String name;
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 15, message = "Password must be between 6 and 15")
+    private String password;
+    private String profileImageUrl;
+
+}

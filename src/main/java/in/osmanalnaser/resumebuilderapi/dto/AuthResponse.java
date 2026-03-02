9337e83f99b15b8bbef698fd491d4e7287aa6899
osman <osman.n2@icloud.com>
2026-03-02 02:36:55 +0100

    feat: add AuthResponse DTO with user profile fields

---
+++ src/main/java/in/osmanalnaser/resumebuilderapi/dto/AuthResponse.java
@@ -0,0 +1,25 @@
+package in.osmanalnaser.resumebuilderapi.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthResponse {
+
+    private String id;
+    private String name;
+    private String email;
+    private String profileImageUrl;
+    private String subscriptionPlan;
+    private Boolean emailVerified;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
